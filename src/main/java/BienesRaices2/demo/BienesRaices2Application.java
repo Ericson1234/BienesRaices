8304a21c8c95d3b28783361d752e2e9ee3e803bf
dpicone28 <dpicone28@gmail.com>
2022-02-13 00:06:46 -0600

    Tarea 1 Bienes Raices

---
+++ src/main/java/BienesRaices2/demo/BienesRaices2Application.java
@@ -0,0 +1,13 @@
+package BienesRaices2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BienesRaices2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BienesRaices2Application.class, args);
+	}
+
+}
